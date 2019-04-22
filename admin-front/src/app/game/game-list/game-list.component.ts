f6ee23841931a7181d9a7fc48176458e34ba2e75
Tomek <tomasz.mackiewicz79@gmail.com>
2019-04-22 09:22:41 +0200

    Game list table

--- admin-front/src/app/game/game-list/game-list.component.ts
+++ admin-front/src/app/game/game-list/game-list.component.ts
@@ -1,14 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { merge, Observable, of as observableOf } from 'rxjs';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { SelectionModel } from '@angular/cdk/collections';
 
 @Component({
     selector: 'app-game-list',
-    templateUrl: './game-list.component.html'
+    templateUrl: './game-list.component.html',
 })
-export class GameListComponent implements OnInit {
+export class GameListComponent implements AfterViewInit {
+    displayedColumns: string[] = ['select', 'created', 'state', 'number', 'title'];
+    exampleDatabase: ExampleHttpDatabase | null;
+    data: GithubIssue[] = [];
+    selection = new SelectionModel<GithubIssue>(true, []);
 
-    constructor() { }
+    resultsLength = 0;
+    isLoadingResults = true;
+    isRateLimitReached = false;
 
-    ngOnInit() {
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+
+    constructor(private http: HttpClient) {}
+
+    ngAfterViewInit() {
+        this.exampleDatabase = new ExampleHttpDatabase(this.http);
+        this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+        merge(this.sort.sortChange, this.paginator.page)
+            .pipe(
+                startWith({}),
+                switchMap(() => {
+                    this.isLoadingResults = true;
+                    return this.exampleDatabase!.getRepoIssues(
+                        this.sort.active, this.sort.direction, this.paginator.pageIndex);
+                }),
+                map(data => {
+                    this.isLoadingResults = false;
+                    this.isRateLimitReached = false;
+                    this.resultsLength = data.total_count;
+
+                    return data.items;
+                }),
+                catchError(() => {
+                    this.isLoadingResults = false;
+                    this.isRateLimitReached = true;
+                    return observableOf([]);
+                })
+            ).subscribe(data => this.data = data);
     }
 
+    isAllSelected() {
+        const numSelected = this.selection.selected.length;
+        const numRows = this.data.length;
+        return numSelected === numRows;
+    }
+
+    masterToggle() {
+        this.isAllSelected() ?
+            this.selection.clear() :
+            this.data.forEach(row => this.selection.select(row));
+    }
+
+    applyFilter(filterValue: string) {
+        //this.data.filter = filterValue.trim().toLowerCase();
+
+        if (this.paginator) {
+            this.paginator.firstPage();
+        }
+    }
+      
+}
+
+export interface GithubApi {
+    items: GithubIssue[];
+    total_count: number;
+}
+
+export interface GithubIssue {
+    created_at: string;
+    number: string;
+    state: string;
+    title: string;
+}
+
+export class ExampleHttpDatabase {
+    constructor(private http: HttpClient) {}
+
+    getRepoIssues(sort: string, order: string, page: number): Observable<GithubApi> {
+        const href = 'https://api.github.com/search/issues';
+        const requestUrl =
+            `${href}?q=repo:angular/material2&sort=${sort}&order=${order}&page=${page + 1}`;
+
+        return this.http.get<GithubApi>(requestUrl);
+    }
 }
--- admin-front/src/app/game/game.module.ts
+++ admin-front/src/app/game/game.module.ts
@@ -4,13 +4,30 @@ import { CommonModule } from '@angular/common';
 import { GameListComponent } from './game-list/game-list.component';
 import { GameAddComponent } from './game-add/game-add.component';
 
+import { 
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
+} from '@angular/material';
+
 @NgModule({
-  declarations: [
-      GameListComponent,
-      GameAddComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        GameListComponent,
+        GameAddComponent
+    ],
+    imports: [
+        CommonModule,
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatTableModule,
+        MatSortModule,
+        MatCheckboxModule,
+        MatFormFieldModule,
+        MatInputModule
+    ]
 })
 export class GameModule { }
