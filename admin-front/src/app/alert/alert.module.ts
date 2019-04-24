eb59ab4a607edd8dd81a371d61eb07e8cdb47b29
Tomek <tomasz.mackiewicz79@gmail.com>
2019-04-24 21:03:51 +0200

    Alert module

---
+++ admin-front/src/app/alert/alert.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { AlertComponent } from './alert.component';
+
+import { AlertService } from './alert.service';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })        
+    ],
+    declarations: [
+        AlertComponent
+    ],
+    providers: [
+        AlertService
+    ],
+    exports: [
+        AlertComponent
+    ]
+})
+
+export class AlertModule {}
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
+}
