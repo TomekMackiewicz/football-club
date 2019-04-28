0856af3088f1156595b13995342dc633adcded2e
Tomek <tomasz.mackiewicz79@gmail.com>
2019-04-28 11:26:26 +0200

    Fix router animation

--- admin-front/src/app/constants/animations.ts
+++ admin-front/src/app/constants/animations.ts
@@ -8,22 +8,40 @@ export const indicatorRotate = trigger('indicatorRotate', [
     )
 ]);
   
-export const fadeAnimation = trigger('fadeAnimation', [
-    transition('* => *', [
-        query(
-            ':enter',
-            [style({ opacity: 0 })],
-            { optional: true }
-        ),
-        query(
-            ':leave',
-            [style({ opacity: 1 }), animate('0.5s', style({ opacity: 0 }))],
-            { optional: true }
-        ),
-        query(
-            ':enter',
-            [style({ opacity: 0 }), animate('0.5s', style({ opacity: 1 }))],
-            { optional: true }
-        )
-    ])
+//export const fadeAnimation = trigger('fadeAnimation', [
+//    transition('* => *', [
+//        query(
+//            ':enter',
+//            [style({ opacity: 0 })],
+//            { optional: true }
+//        ),
+//        query(
+//            ':leave',
+//            [style({ opacity: 1 }), animate('0.5s', style({ opacity: 0 }))],
+//            { optional: true }
+//        ),
+//        query(
+//            ':enter',
+//            [style({ opacity: 0 }), animate('0.5s', style({ opacity: 1 }))],
+//            { optional: true }
+//        )
+//    ])
+//]);
+
+export const fadeAnimation =
+  trigger('fadeAnimation', [
+    transition('* <=> *', [
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          right: '16px',
+          left: '16px',
+          opacity: 0,
+          transform: 'scale(0) translateY(100%)',
+        }),
+      ], { optional: true }),
+      query(':enter', [
+        animate('600ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
+      ], { optional: true })
+    ]),
 ]);
