9c414ccb8ec77e8809f63271473ff39bc5e32b13
Tomek <tomasz.mackiewicz79@gmail.com>
2019-05-09 22:37:20 +0200

    Spinner on http request OR route change

--- admin-front/src/app/shared/loader/loader.component.ts
+++ admin-front/src/app/shared/loader/loader.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Router, 
+         Event, 
+         NavigationStart, 
+         NavigationEnd, 
+         NavigationCancel, 
+         NavigationError 
+} from '@angular/router';
 import { LoaderService } from '../../services/loader.service';
     
 @Component({
     selector: 'app-loader',
     templateUrl: './loader.component.html'
 })
-export class LoaderComponent {  
+export class LoaderComponent {
+        
     isLoading: Subject<boolean> = this.loaderService.isLoading;
-    constructor(private loaderService: LoaderService){}  
+    routeChanged: boolean = false;
+    
+    constructor(
+        private loaderService: LoaderService,
+        private router: Router
+    ) {
+        this.router.events.subscribe((event: Event) => {
+            switch (true) {
+                case event instanceof NavigationStart: {
+                    this.routeChanged = true;
+                    break;
+                }
+
+                case event instanceof NavigationEnd:
+                case event instanceof NavigationCancel:
+                case event instanceof NavigationError: {
+                    setTimeout(() => { this.routeChanged = false; }, 200);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        });
+    }
+      
 }
