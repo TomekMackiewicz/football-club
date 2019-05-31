dc84b2a1942c2d425549f8043bd2a04f374d139f
Tomek <tomasz.mackiewicz79@gmail.com>
2019-06-01 00:59:23 +0200

    Refactor category tests

--- admin-api/tests/Controller/CategoryControllerTest.php
+++ admin-api/tests/Controller/CategoryControllerTest.php
@@ -1,6 +1,5 @@
 <?php
-
-// ./bin/phpunit
+//declare(strict_types=1);
 
 namespace App\Tests\Controller;
 
@@ -8,50 +7,64 @@ use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
+use GuzzleHttp\Client;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class CategoryControllerTest extends WebTestCase
 { 
-    private static $apiUrl = 'http://localhost:8000/api/v1/category';
+    private $apiUrl;
     private $client;
+    protected static $kernel;
+    private static $application;
     
     public function __construct()
     {
         parent::__construct();
-        $this->client = new \GuzzleHttp\Client();
+        self::$kernel = new Kernel('test', true);
+        self::$kernel->boot();
+        self::$application = new Application(self::$kernel);
+        $this->client = new Client();
+        $this->apiUrl = self::$kernel->getContainer()->getParameter('base_url');       
     }
 
     public static function setUpBeforeClass(): void
     {
-        self::buildDb();
+        self::buildDb(self::$kernel, self::$application);
     }
     
-    public function testGetCategoriesEmptyResult()
+    public static function tearDownAfterClass(): void
     {
-        $response = $this->client->get(self::$apiUrl.'?sort=name&order=desc&page=1&size=10&filters={"name":""}');
+        self::clearSchema(self::$application);
+    }
+    
+    public function testGetCategoriesEmptyResult(): void
+    {
+        $response = $this->client->get($this->apiUrl.'/categories?sort=name&order=desc&page=1&size=10&filters={"name":""}');
         $this->assertEquals(204, $response->getStatusCode());
     }
 
-    public function testAddNewCategory()
+    public function testAddNewCategory(): void
     {        
-        $response = $this->client->post(self::$apiUrl, [
+        $response = $this->client->post($this->apiUrl.'/categories', [
             'json' => [
                 'name' => 'Default'
              ]
-        ]); 
+        ]);
         $msg = json_decode($response->getBody(true), true);
+        //$msg = (string) $response->getBody();
         $this->assertEquals(201, $response->getStatusCode());
         $this->assertEquals('category.added', $msg);
     }
 
-    public function testGetCategories()
+    public function testGetCategories(): void
     {
-        $response = $this->client->get(self::$apiUrl.'?sort=name&order=desc&page=1&size=10&filters={"name":""}');
+        $response = $this->client->get($this->apiUrl.'/categories?sort=name&order=desc&page=1&size=10&filters={"name":""}');
         $this->assertEquals(200, $response->getStatusCode());
     }    
 
-    public function testAddNewCategoryNullValuesRegex()
+    public function testAddNewCategoryNullValuesRegex(): void
     {        
-        $response = $this->client->post(self::$apiUrl, [
+        $response = $this->client->post($this->apiUrl.'/categories', [
             'json' => [
                 'name' => ''
              ],
@@ -63,9 +76,9 @@ class CategoryControllerTest extends WebTestCase
         
     }
     
-    public function testEditCategory()
+    public function testEditCategory(): void
     {       
-        $response = $this->client->patch(self::$apiUrl.'/1', [
+        $response = $this->client->patch($this->apiUrl.'/categories/1', [
             'json' => [
                 'id' => 1,
                 'name' => 'New' 
@@ -76,9 +89,9 @@ class CategoryControllerTest extends WebTestCase
         $this->assertEquals('category.edited', $msg);        
     }
     
-    public function testDeleteCategory()
+    public function testDeleteCategory(): void
     {
-        $response = $this->client->delete(self::$apiUrl, [
+        $response = $this->client->delete($this->apiUrl.'/categories', [
             'json' => [1,2,3]
         ]); 
         $msg = json_decode($response->getBody(true), true);
@@ -86,36 +99,30 @@ class CategoryControllerTest extends WebTestCase
         $this->assertEquals('categories.deleted', $msg);         
     }
 
-    private static function buildDb()
-    {
-        $kernel = new Kernel('test', true);
-        $kernel->boot();
-        $application = new Application($kernel);
-        $application->setAutoExit(false);    
+    private static function buildDb($kernel, $application): void
+    {        
+        $application->setAutoExit(false);
+        $doctrine = $kernel->getContainer()->get('doctrine');       
+        $schemaManager = $doctrine->getConnection()->getSchemaManager();
+        
+        if ($schemaManager->tablesExist(array('category')) === false) {
+            $application->run(new ArrayInput(array(
+                'doctrine:schema:drop',
+                '--force' => true
+            )));
 
+            $application->run(new ArrayInput(array(
+                'doctrine:schema:create'
+            )));            
+        }
+    }
+    
+    private static function clearSchema($application): void
+    {
+        $application->setAutoExit(false);
         $application->run(new ArrayInput(array(
             'doctrine:schema:drop',
             '--force' => true
-        )));
-
-        $application->run(new ArrayInput(array(
-            'doctrine:schema:create'
-        )));
-
-//        $application->run(new ArrayInput(array(
-//            'fos:user:create', 
-//            'username' => 'admin', 
-//            'email' => 'admin@test.com', 
-//            'password' => 'admin', 
-//            '--super-admin' =>true
-//        )));    
-//
-//        $application->run(new ArrayInput(array(
-//            'fos:user:create', 
-//            'username' => 'user', 
-//            'email' => 'user@test.com', 
-//            'password' => 'user'
-//        )));    
-
-    }   
+        )));        
+    }
 }       
