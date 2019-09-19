dbb1b54062a31a128c3a68ad314b0010ca344600
Tomek <tomasz.mackiewicz79@gmail.com>
2019-09-19 18:53:31 +0200

    Token interceptor

--- admin-front/src/app/interceptors/loader.interceptor.ts
+++ admin-front/src/app/interceptors/loader.interceptor.ts
@@ -6,7 +6,9 @@ import { LoaderService } from '../services/loader.service';
 
 @Injectable()
 export class LoaderInterceptor implements HttpInterceptor {
+
     constructor(public loaderService: LoaderService) { }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         this.loaderService.show();
         return next.handle(req).pipe(
