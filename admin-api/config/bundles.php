9a34daef7664f1b93c3a4a01bc9bff0e7d8d8144
Tomek <tomasz.mackiewicz79@gmail.com>
2019-10-15 21:41:02 +0200

    Refresh token

--- admin-api/config/bundles.php
+++ admin-api/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
